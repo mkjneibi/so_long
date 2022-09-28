7a0225204b83f07efcf7005e02d32f965d2dd512
Meera Aljneibi <mealjnei@lab3r6s2.42abudhabi.ae>
2022-09-28 17:30:28 +0400

    default

--- so_long.c
+++ so_long.c
@@ -3,31 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mealjnei <mealjnei@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: mealjnei <mealjnei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 17:28:37 by mealjnei          #+#    #+#             */
-/*   Updated: 2022/09/28 16:42:45 by mealjnei         ###   ########.fr       */
+/*   Updated: 2022/09/28 17:27:46 by mealjnei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	exit_window(int key, t_game *f)
+int	exit_window(int key, t_game *game)
 {
-	if (key == 12)
-	{
+	int x = 25;
+	if (key == 53)
 		exit(1);
+	if (key == A_KEY)
+	{
+		game->x -= x;
+		mlx_clear_window(game->mlx, game->win);
+		mlx_put_image_to_window(game->mlx,game->win, game->img.img_ptr, game->x, game->y);
+	}
+	if (key == D_KEY)
+	{
+		game->x += x;
+		mlx_clear_window(game->mlx, game->win);
+		mlx_put_image_to_window(game->mlx,game->win, game->img.img_ptr, game->x, game->y);
+	}
+	if (key == W_KEY)
+	{
+		game->y -= x;
+		mlx_clear_window(game->mlx, game->win);
+		mlx_put_image_to_window(game->mlx,game->win, game->img.img_ptr, game->x, game->y);
+	}
+	if (key == S_KEY)
+	{
+		game->y += x;
+		mlx_clear_window(game->mlx, game->win);
+		mlx_put_image_to_window(game->mlx,game->win, game->img.img_ptr, game->x, game->y);
 	}
 	return (0);
 }
 
 void	so_long(t_game *game)
 {
-	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, 560, 620, "42game");
-	mlx_key_hook(game->win, exit_window, game);
-	mlx_loop(game->mlx);
+	char 	*str;
 
+	str = ft_strdup("./player_2.xpm");
+	game->x = 1;
+	game->y = 1;
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, 1000, 1000, "42game");
+	game->img.img_ptr = mlx_xpm_file_to_image(game->mlx, str, &game->x, &game->y);
+	mlx_put_image_to_window(game->mlx,game->win, game->img.img_ptr, game->x, game->y);
+	mlx_hook(game->win, 2, 1L << 0, exit_window, game);
+	mlx_loop(game->mlx);
 	// game->i =
 	// game->img;
 }
--- so_long.h
+++ so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mealjnei <mealjnei@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: mealjnei <mealjnei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 16:48:55 by mealjnei          #+#    #+#             */
-/*   Updated: 2022/09/28 16:03:04 by mealjnei         ###   ########.fr       */
+/*   Updated: 2022/09/28 17:14:30 by mealjnei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,27 @@
 # include "libft/includes/libft.h"
 # include "mlx/mlx.h"
 
+# ifdef __MACH__
+#  define A_KEY 0
+#  define D_KEY 2
+#  define S_KEY 1
+#  define W_KEY 13
+#  define ESC_KEY 53
+#  define SPACE_KEY 49
+# endif
+
 typedef struct	s_img
 {
 	void		*img_ptr;
-	int			*data;
-	int			size_l;
-	int			bpp;
-	int			endian;
 }           t_img;
 
 typedef struct	s_game
 {
 	void		*mlx;
     void		*win;
-    int 		i;
+    int			i;
+	int	 		x;
+	int			y;
 	t_img       img;
 }           t_game;
 
